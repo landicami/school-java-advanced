0c1d6fe2841ec46a1cd0ac6a04b907242ca6fa9b
Camilla Landin <landin.camilla8@gmail.com>
2024-05-03 09:00:55 +0200

    Todo app with onclick, state, delete and so on

--- 02-react-simple-todos/src/App.tsx
+++ 02-react-simple-todos/src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import "./App.css";
+//VARJE GÅNG EN STATEUPPDATERING SKER GÖRS EN OMRENDERING
 
 interface Todo {
 	id: number;
@@ -24,17 +26,79 @@ function App() {
 	}
 	setTodos([...todos, newTodo]);
 	setInputTodo("");
+  }
+
+  const handleTodoClick = (todo: Todo) => {
+	todo.completed = !todo.completed;
+	console.log(todo);
+	setTodos([...todos]);
 
   }
 
+  const completedTodos = todos.filter(todo => todo.completed)
+
+  const handleDeletedTodo = (todoToDelete: Todo) => {
+	setTodos(todos.filter(todo => todo !== todoToDelete))
+
+  }
+
+  const notCompletedinlist = todos.filter(todo => !todo.completed);
+  const completedinList = todos.filter(todo => todo.completed);
+
+
+
   return (
     <div className="container">
-	<h1>I will always todo you</h1>
+		<h1>I will always todo you</h1>
 
+		{todos.length === 0 &&
+		<p>The todos are none
+			</p>
+			}
+		{todos.length > 0 &&
+		<>
+		<h2>There are this many todos: {todos.length}</h2>
+		<h3>{completedTodos.length} är avklarade</h3>
 		<ul className='mt-5'>
-			{todos.map(todo =>
-			<li key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
-			{todo.title}
+			<h4>Not Completed</h4>
+			{notCompletedinlist.map(todo =>
+				<li
+				className = {todo.completed ? "done" : ""}
+				key = {todo.id}
+				>
+
+				{todo.title}
+
+					<button
+					className='btn btn-warning btn-sm'
+					onClick={() => (handleTodoClick(todo))}>{todo.completed ? "Undone" : "Done"}</button>
+					<button
+							className="btn btn-warning btn-sm ms-1"
+							onClick={() => handleDeletedTodo(todo)}
+						>🗑️</button>
+
+				</li>
+			)}
+		</ul>
+		<ul className='mt-5'>
+		<h4>Completed</h4>
+
+			{completedinList.map(todo =>
+				<li
+				className = {todo.completed ? "done" : ""}
+				key = {todo.id}
+				>
+
+				{todo.title}
+
+					<button
+					className='btn btn-warning btn-sm'
+					onClick={() => (handleTodoClick(todo))}>{todo.completed ? "Undone" : "Done"}</button>
+					<button
+							className="btn btn-warning btn-sm ms-1"
+							onClick={() => handleDeletedTodo(todo)}
+						>🗑️</button>
+
 				</li>
 			)}
 		</ul>
@@ -52,6 +116,8 @@ function App() {
 				className='btn btn-primary'>Create a new todo</button>
 			</div>
 		</form>
+		</>
+		}
     </div>
   )
 }
