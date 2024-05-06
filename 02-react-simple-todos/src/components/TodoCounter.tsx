56542781ce846d191e4453f4ba5801146dabc45b
Camilla Landin <landin.camilla8@gmail.com>
2024-05-06 10:42:09 +0200

    Minor fix

--- 02-react-simple-todos/src/components/TodoCounter.tsx
+++ 02-react-simple-todos/src/components/TodoCounter.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Todo } from '../types/Todo'
 
+//kan också skicka med ett nummeravtal
 interface Todocounter {
 	todos: Todo[]
 }
 
 const TodoCounter: React.FC<Todocounter> = ({ todos }) => {
+	//undvik att göra något mer än en gång
 	const completedTodos = todos.filter(todo => todo.completed)
 
   return (
