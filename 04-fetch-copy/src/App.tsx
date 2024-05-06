9935f49f71ff08c45601691187e366e83f7e5695
Camilla Landin <landin.camilla8@gmail.com>
2024-05-06 11:44:09 +0200

    adding code for fetching stuff

--- 04-fetch-copy/src/App.tsx
+++ 04-fetch-copy/src/App.tsx
@@ -3,8 +3,33 @@ import { Resource } from "./types/Resource";
 import "./assets/scss/App.scss";
 
 function App() {
-	const [resource, setResource] = useState('posts');
+	const [resource, setResource] = useState('');
 	const [data, setData] = useState<Resource[]>([]);
+
+	// fetch("https://jsonplaceholder.typicode.com/posts")
+	// .then(res => res.json())
+	// .then(data => setData(data)); detta gör tusen omrenderingar för att vi ändrar en state varje gång vi hämtar
+
+	// useEffect(()=> {
+	// 	fetch(`https://jsonplaceholder.typicode.com/${resource}`)
+	// 	.then(res => res.json())
+	// 	.then(data => setData(data));
+	// }, [resource]) //glöm inte att ge den ett värde som den ska bevaka , en dependencie array
+
+	//vi kan inte göra effectfunktionen async på annat sätt
+	useEffect( () => {
+		if(!resource){
+			return;
+		}
+		const fetchedData = async () => {
+			console.log("Side-effect triggered due to resource changing value to:", resource);
+			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`);
+			const payload = await res.json();
+			setData(payload);
+		}
+	fetchedData();
+
+	}, [resource]);
 
 	return (
 		<div className="container">
@@ -24,7 +49,7 @@ function App() {
 
 					<ol>
 						{data.map(item => (
-							<li>{item.title}</li>
+							<li key={item.id}>{item.title}</li>
 						))}
 					</ol>
 				</>
