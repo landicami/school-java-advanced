03713616f596af63e92e77478a2c506638dbc45c
Camilla Landin <landin.camilla8@gmail.com>
2024-05-06 15:20:24 +0200

    Moving stuff to a component

--- 04-fetch-copy/src/App.tsx
+++ 04-fetch-copy/src/App.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { Resource } from "./types/Resource";
 import "./assets/scss/App.scss";
+import Data from "./components/Data";
+import { getResource } from "./services/JSONPlanceholder";
 
 function App() {
 	const [resource, setResource] = useState('');
 	const [data, setData] = useState<Resource[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState(false);
 
 	// fetch("https://jsonplaceholder.typicode.com/posts")
 	// .then(res => res.json())
@@ -21,40 +25,56 @@ function App() {
 		if(!resource){
 			return;
 		}
-		const fetchedData = async () => {
-			console.log("Side-effect triggered due to resource changing value to:", resource);
-			const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`);
-			const payload = await res.json();
-			setData(payload);
-		}
-	fetchedData();
 
+		const fetchData = async () => {
+				//här kan vi också tömma data setData([])
+				setData([]);
+				setIsError(false);
+				setIsLoading(true);
+			try {
+				// const res = await fetch(`https://jsonplaceholder.typicode.com/${resource}`);
+
+				// if(!res.ok){
+				// 	setIsError(true);
+				// 	return;
+				// };
+
+				// const payload = await res.json();
+				// await new Promise(r => setTimeout(r, 2500));
+				const payload = await getResource(resource);
+
+				setData(payload);
+			} catch (err) {
+				setIsError(true);
+				console.log(err);
+			};
+
+			setIsLoading(false);
+			};
+		fetchData();
 	}, [resource]);
+
 
 	return (
 		<div className="container">
-			<h1 className="mb-3">Fetch</h1>
+		<h1 className="mb-3">Fetch</h1>
 
 			<div className="d-flex justify-content-between mb-5">
 				<button onClick={() => setResource('albums')} className="btn btn-primary">Albums</button>
 				<button onClick={() => setResource('photos')} className="btn btn-success">Photos</button>
 				<button onClick={() => setResource('posts')} className="btn btn-warning">Posts</button>
 				<button onClick={() => setResource('todos')} className="btn btn-danger">Todos</button>
+				<button onClick={() => setResource('memes')} className="btn btn-info">Memes</button>
+
 			</div>
+				< Data
+				data={data}
+				resource={resource}
+				isLoading={isLoading}
+				isError={isError}
+				/>
+				</div>
 
-			{data && (
-				<>
-					<h2>{resource}</h2>
-					<p>There are {data.length} {resource}.</p>
-
-					<ol>
-						{data.map(item => (
-							<li key={item.id}>{item.title}</li>
-						))}
-					</ol>
-				</>
-			)}
-		</div>
 	)
 }
 
