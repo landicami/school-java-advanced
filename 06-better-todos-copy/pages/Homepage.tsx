a18c013239047c90d326aae41a90ece464347619
Camilla Landin <landin.camilla8@gmail.com>
2024-05-10 12:19:13 +0200

    Adding homepage comp.

---
+++ 06-better-todos-copy/pages/Homepage.tsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+  return (
+	<>
+	<h1>Welcome to Better Todos</h1>
+	<p>Because when your life is on fire 🔥, you need a <Link to="/todos">todo list</Link>.</p>
+
+	</>
+  )
+}
+
+export default Homepage;
