762e802621a45ff68f846fa38746483f13684085
Camilla Landin <landin.camilla8@gmail.com>
2024-05-14 15:04:35 +0200

    Link to search from homepage

--- 07-hackernews/src/pages/Homepage.tsx
+++ 07-hackernews/src/pages/Homepage.tsx
@@ -1,11 +1,12 @@
 import Container from "react-bootstrap/Container"
+import { Link } from "react-router-dom"
 
 const Homepage = () => {
 
   return (
 	<Container>
 		<h1>Welcome to home</h1>
-		{/* <p>{apiStuff}</p> */}
+		<p>Do you wanna search the internet for hacking news, please <Link to={"/search"}>press me!</Link></p>
 	</Container>
   )
 }
