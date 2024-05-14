fd8cb07431e5a8df210176eb1b84e2a3cd03c42f
Camilla Landin <landin.camilla8@gmail.com>
2024-05-14 10:42:44 +0200

    Installing searchpage

---
+++ 07-hackernews/src/pages/Searchpage.tsx
@@ -0,0 +1,10 @@
+import  Container  from "react-bootstrap/Container"
+const Searchpage = () => {
+  return (
+	<Container>
+		<h1>This is where u search</h1>
+	</Container>
+  )
+}
+
+export default Searchpage
