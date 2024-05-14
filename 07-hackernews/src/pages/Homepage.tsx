3c7652be54d841f98fa1478612ef9677141c2fea
Camilla Landin <landin.camilla8@gmail.com>
2024-05-14 13:05:27 +0200

    Removing small logic

--- 07-hackernews/src/pages/Homepage.tsx
+++ 07-hackernews/src/pages/Homepage.tsx
@@ -1,8 +1,11 @@
 import Container from "react-bootstrap/Container"
+
 const Homepage = () => {
+
   return (
 	<Container>
 		<h1>Welcome to home</h1>
+		{/* <p>{apiStuff}</p> */}
 	</Container>
   )
 }
