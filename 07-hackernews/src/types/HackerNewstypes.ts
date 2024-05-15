4f7517144f195f273d0a49bfe8b11f9b3fda4965
Camilla Landin <landin.camilla8@gmail.com>
2024-05-15 10:28:04 +0200

    types to be as I want them, I dont have to use all

--- 07-hackernews/src/types/HackerNewstypes.ts
+++ 07-hackernews/src/types/HackerNewstypes.ts
@@ -5,31 +5,16 @@ interface HighlightResult {
 }
 
 interface HackerHit {
-    _highlightResult: {
-        author: HighlightResult;
-        comment_text: HighlightResult;
-        title: HighlightResult;
-        url: HighlightResult;
-    };
-    _tags: string[];
     author: string;
-    comment_text: string;
     created_at: string;
     created_at_i: number;
     objectID: string;
     points: number;
-    story_id: number;
+	story_text?: string;
     title: string;
     url: string;
-    updated_at: string;
 }
 
 export interface HackerResponse {
-    exhaustive: {
-        nbHits: boolean;
-        typo: boolean;
-    };
-    exhaustiveNbHits: boolean;
-    exhaustiveTypo: boolean;
     hits: HackerHit[];
 }
