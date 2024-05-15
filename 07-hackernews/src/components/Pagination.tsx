a96e158f80d3e3626bf55d5db020d73d8c7f90f1
Camilla Landin <landin.camilla8@gmail.com>
2024-05-15 15:32:44 +0200

    adding pagination component with props

---
+++ 07-hackernews/src/components/Pagination.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Button from "react-bootstrap/Button";
+import { HackerResponse } from '../types/HackerNewstypes';
+interface PaginationProps {
+	page: number;
+	backPage: () => void;
+	addpage: () => void;
+	searchNews: HackerResponse | null
+}
+const Pagination:React.FC<PaginationProps> = ({page, addpage, backPage, searchNews}) => {
+  return (
+	<div className="d-flex justify-content-between align-items-center">
+						<div className="prev">
+							<Button
+							variant="primary"
+							onClick={backPage}
+							disabled={page === 0}
+							>{page === 0 ? "No previous pages" : "Previous Page"}</Button>
+						</div>
+
+						<div className="page">{searchNews && searchNews.page + 1}</div>
+
+						<div className="next">
+							<Button
+							variant="primary"
+							onClick={addpage}
+							disabled={!searchNews || (searchNews.hits.length === 0)}
+							>
+								{(!searchNews || (searchNews.hits.length === 0)) ? "No more pages" : "Next Page"}
+							</Button>
+						</div>
+					</div>
+  )
+}
+
+export default Pagination
