4d1c1f0ce611d4aa9fcc865261cc44cd117da27d
Camilla Landin <landin.camilla8@gmail.com>
2024-06-17 15:01:04 +0200

    todoscollection

--- 21-firebase-todos-copy/src/services/firebase.ts
+++ 21-firebase-todos-copy/src/services/firebase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { CollectionReference, collection, getFirestore } from "firebase/firestore";
+import { Todo } from "../types/Todo.types";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +19,8 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const databas = getFirestore(app);
+
+export const todosCol = collection(databas, "todos") as CollectionReference<Todo>
 //this is my databas
 
 export default app;
