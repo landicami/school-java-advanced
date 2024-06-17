26bba0853d88d56853022192b4ccd0d760eb1e1e
Camilla Landin <landin.camilla8@gmail.com>
2024-06-17 09:06:37 +0200

    edit author

--- 20-createauhtor tanstack form/src/pages/EditAuthorPage.tsx
+++ 20-createauhtor tanstack form/src/pages/EditAuthorPage.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import Card from "react-bootstrap/Card";
+import { useParams } from "react-router-dom";
+import WarningAlert from "../components/alerts/WarningAlert";
+import useAuthor from "../hooks/useAuthor";
+import CreateAuthorForm from "../components/forms/CreateAuthorForm";
+import AuthorForm from "../components/forms/AuthorForm";
 
 const EditAuthorPage = () => {
-  return (
-	<div>EditAuthorPage</div>
-  )
-}
+	const { id } = useParams();
+	const authorId = Number(id);
+	const { data: author, isError, isLoading } = useAuthor(authorId);
 
-export default EditAuthorPage
+	return (
+		<>
+			{isError && (
+				<WarningAlert>
+					An terrible, inexplicable error occurred while fetching authors. It wasn't me!
+				</WarningAlert>
+			)}
+
+			{isLoading && <p>Loading author...</p>}
+
+			{author && (
+				<>
+					<h1 className="mb-3">Edit: {author.name}</h1>
+
+					<Card>
+						<Card.Body>
+							<Card.Title>Change Author</Card.Title>
+							<AuthorForm author={author} />
+						</Card.Body>
+					</Card>
+				</>
+			)}
+
+		</>
+	);
+};
+
+export default EditAuthorPage;
