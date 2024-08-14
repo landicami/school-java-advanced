7dd035c7ed0f3d915d2268bdb582e64ed451c194
Camilla Landin <landin.camilla8@gmail.com>
2024-08-14 09:10:45 +0200

    react hook form

--- 21-firebase-todos-copy/src/pages/EditTodoPage.tsx
+++ 21-firebase-todos-copy/src/pages/EditTodoPage.tsx
@@ -1,44 +1,85 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useNavigate, useParams } from "react-router-dom";
-import { Todo } from "../types/Todo.types";
-
-const todo: Todo = {
-	_id: "133713371337",
-	title: "Learn to fake better data 😅",
-	completed: true,
-};
+import { Todo, TodoFormData } from "../types/Todo.types";
+import useGetTodo from "../hooks/useGetTodo";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { doc, updateDoc } from "firebase/firestore";
+import { todosCol } from "../services/firebase";
+import { toast } from "react-toastify";
 
 const EditTodoPage = () => {
 	const [inputNewTodoTitle, setInputNewTodoTitle] = useState("");
 	const { id } = useParams();
-	const todoId = Number(id);
+	const todoId = id;
 	const navigate = useNavigate();
+	const { getSingleData: getTodo, data: todo, loading, error } = useGetTodo(todoId!);
 
-	const handleSubmit = async (e: React.FormEvent) => {
-		e.preventDefault();
+	const {
+		handleSubmit,
+		register,
+		formState: { errors },
+	} = useForm<TodoFormData>();
 
-		if (!todo) {
-			return;
-		}
+	const onFormSubmit: SubmitHandler<TodoFormData> = async (data) => {
+		const todoRef = doc(todosCol, todoId);
+		await updateDoc(todoRef, {
+			title: data.title,
+		});
+		console.log(todoRef);
+		getTodo();
+		toast.success("Yay you updated, going back in two");
 
-		// Update the todo
-
-		// Redirect user back to /todos/:id
-		// navigate(`/todos/${todoId}`);
+		setTimeout(() => {
+			navigate("/todos");
+		}, 2000);
 	};
+
+	useEffect(() => {
+		getTodo();
+	}, [todoId]);
 
 	return (
 		<>
-			<h1 title={`Todo #${todoId}`}>Edit: {todo.title}</h1>
+			{todo && <h1 title={`Todo #${todoId}`}>Edit: {todo.title}</h1>}
 
-			<Form onSubmit={handleSubmit} className="mb-3">
+			<Form onSubmit={handleSubmit(onFormSubmit)} className="mb-3">
+				<Form.Control
+					type="text"
+					className="form-control"
+					aria-label="The title of the new todo"
+					placeholder={todo ? todo.title : "Write here"}
+					{...register("title", {
+						required: true,
+						minLength: {
+							value: 5,
+							message: "MOAR CHARACHTERS at least five",
+						},
+					})}
+				/>
+
+				{errors.title && (
+					<div className="form-text text-danger">
+						{errors.title.message ?? "Please enter some chars or more"}
+					</div>
+				)}
+
+				<Button variant="primary" type="submit">
+					Save
+				</Button>
+			</Form>
+
+			<Button variant="secondary" onClick={() => navigate(-1)}>
+				&laquo; Go back
+			</Button>
+
+			{/* <Form onSubmit={handleSubmit} className="mb-3">
 				<Form.Group className="mb-3" controlId="title">
 					<Form.Label>Title</Form.Label>
 					<Form.Control
 						type="text"
-						placeholder="Enter the new title"
+						placeholder={todo ? todo.title : "Write here"}
 						onChange={(e) => setInputNewTodoTitle(e.target.value)}
 						value={inputNewTodoTitle}
 					/>
@@ -51,7 +92,7 @@ const EditTodoPage = () => {
 
 			<Button variant="secondary" onClick={() => navigate(-1)}>
 				&laquo; Go back
-			</Button>
+			</Button> */}
 		</>
 	);
 };
