9f827398a6638fd49e34b7bfd534fea55e8c5f71
Camilla Landin <landin.camilla8@gmail.com>
2024-08-14 09:11:17 +0200

    adding timestamp to type

--- 21-firebase-todos-copy/src/types/Todo.types.ts
+++ 21-firebase-todos-copy/src/types/Todo.types.ts
@@ -1,12 +1,16 @@
+import { Timestamp } from "firebase/firestore";
+
 export interface Todo {
 	_id: string;
 	title: string;
 	completed: boolean;
+	timestamp?: Timestamp;
 }
 
-export type NewTodo = Omit<Todo, "_id">;
+export type NewTodo = Omit<Todo, "_id"> & { timestamp: Timestamp };
 
 export type TodoFormData = {
 	title: string;
 	completed: boolean;
+	timestamp: Timestamp;
 };
